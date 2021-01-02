2a2df67c0c203106a6ae4862df4f0b2f578eaf1c
Shikhar Sharma <ceoshikhar@gmail.com>
2021-01-02 17:52:16 +0530

    fix: change code styles on initial document load event and when the href changes, i.e user navigates on Github

--- better-github.js
+++ better-github.js
@@ -1,10 +1,39 @@
-const codeElements = document.getElementsByClassName("blob-code-inner");
-const len = codeElements.length;
+let oldHref = document.location.href;
 
-for (let i = 0; i < len; i++) {
-  // TODO: Instead of hard coding the `fontFamily` and `fontSize`, we should be 
-  // able to allow the user to choose a font size and font family of their 
-  // choice (which they have installed on their machine).
-  codeElements[i].style.fontFamily = "'Fira Code', 'monospace'";
-  codeElements[i].style.fontSize = "14px";
+window.onload = function() {
+  // On the first `onLoad` event we change the styles.
+  changeStyles();
+
+  const bodyList = document.querySelector("body");
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (oldHref !== document.location.href) {
+        oldHref = document.location.href;
+        // When the user navigates on Github to other page, we change the styles
+        // again otherwise the styles will change only when any page on Github
+        // is loaded at first.
+        changeStyles();
+      }
+    });
+  });
+
+  const config = {
+    childList: true,
+    subtree: true
+  }
+
+  observer.observe(bodyList, config);
+};
+
+function changeStyles() {
+  const codeElements = document.getElementsByClassName("blob-code-inner");
+  const len = codeElements.length;
+
+  for (let i = 0; i < len; i++) {
+    // TODO: Instead of hard coding the `fontFamily` and `fontSize`, we should
+    // be able to allow the user to choose a font size and font family of their
+    // choice (which they have installed on their machine).
+    codeElements[i].style.fontFamily = "'Fira Code', 'monospace'";
+    codeElements[i].style.fontSize = "14px";
+  }
 }
