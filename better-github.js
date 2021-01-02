10fdbe33393cfed5844467e2f08c1d80d86f4871
Shikhar Sharma <ceoshikhar@gmail.com>
2021-01-02 23:20:42 +0530

    feat: apply the css styles to the line numbers while viewing the code files

--- better-github.js
+++ better-github.js
@@ -1,8 +1,11 @@
+let DEFAULT_FONT_FAMILY = "'Fira Code', 'monospace'";
+let DEFAULT_FONT_SIZE   = "14px";
+
 let oldHref = document.location.href;
 
 window.onload = function() {
   // On the first `onLoad` event we change the styles.
-  changeStyles();
+  applyStyles();
 
   const bodyList = document.querySelector("body");
   const observer = new MutationObserver((mutations) => {
@@ -12,7 +15,7 @@ window.onload = function() {
         // When the user navigates on Github to other page, we change the styles
         // again otherwise the styles will change only when any page on Github
         // is loaded at first.
-        changeStyles();
+        applyStyles();
       }
     });
   });
@@ -25,22 +28,30 @@ window.onload = function() {
   observer.observe(bodyList, config);
 };
 
-function changeStyles() {
-  const codeElements = document.getElementsByClassName("blob-code-inner");
-  const codeElementsLen = codeElements.length;
+function applyStyles(fontFamily = DEFAULT_FONT_FAMILY,
+                     fontSize   = DEFAULT_FONT_SIZE) {
+  const codeTextElements = document.getElementsByClassName("blob-code-inner");
+  const codeTextElementsLen = codeTextElements.length;
+  const codeLineNumElements = document.getElementsByClassName("blob-num");
+  const codeLineNumElementsLen = codeLineNumElements.length;
   const preElements = document.querySelectorAll("pre");
   const preElementsLen = preElements.length;
 
   // TODO: Instead of hard coding the `fontFamily` and `fontSize`, we should
   // be able to allow the user to choose a font size and font family of their
   // choice (which they have installed on their machine).
-  for (let i = 0; i < codeElementsLen; i++) {
-    codeElements[i].style.fontFamily = "'Fira Code', 'monospace'";
-    codeElements[i].style.fontSize = "14px";
+  for (let i = 0; i < codeTextElementsLen; i++) {
+    codeTextElements[i].style.fontFamily = fontFamily;
+    codeTextElements[i].style.fontSize = fontSize;
+  }
+
+  for (let i = 0; i < codeLineNumElementsLen; i++) {
+    codeLineNumElements[i].style.fontFamily = fontFamily;
+    codeLineNumElements[i].style.fontSize = fontSize;
   }
 
   for (let i = 0; i < preElementsLen; i++ ){
-    preElements[i].style.fontFamily = "'Fira Code', 'monospace'";
-    preElements[i].style.fontSize = "14px";
+    preElements[i].style.fontFamily = fontFamily;
+    preElements[i].style.fontSize = fontSize;
   }
 }
