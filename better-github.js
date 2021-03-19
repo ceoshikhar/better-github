98434fca6914505f7da7c6873dbcbe2f6c499773
Shikhar Sharma <ceoshikhar@gmail.com>
2021-03-19 15:42:57 +0530

    chore: change font to "Hack" and format code

--- better-github.js
+++ better-github.js
@@ -1,17 +1,17 @@
-const DEFAULT_FONT_FAMILY = "'SF Mono', 'monospace'";
-const DEFAULT_FONT_SIZE   = "14px";
+const DEFAULT_FONT_FAMILY = "'Hack', 'monospace'";
+const DEFAULT_FONT_SIZE   = "16px";
 
 // TODO: Instead of hard coding the `fontFamily` and `fontSize`, we should
 // be able to allow the user to choose a font size and font family of their
 // choice (which they have installed on their machine).
 function applyStyles(fontFamily = DEFAULT_FONT_FAMILY,
                      fontSize   = DEFAULT_FONT_SIZE) {
-  const codeTextElements = document.getElementsByClassName("blob-code-inner");
-  const codeTextElementsLen = codeTextElements.length;
-  const codeLineNumElements = document.getElementsByClassName("blob-num");
-  const codeLineNumElementsLen = codeLineNumElements.length;
-  const preElements = document.querySelectorAll("pre");
-  const preElementsLen = preElements.length;
+  const codeTextElements        = document.getElementsByClassName("blob-code-inner");
+  const codeTextElementsLen     = codeTextElements.length;
+  const codeLineNumElements     = document.getElementsByClassName("blob-num");
+  const codeLineNumElementsLen  = codeLineNumElements.length;
+  const preElements             = document.querySelectorAll("pre");
+  const preElementsLen          = preElements.length;
 
   for (let i = 0; i < codeTextElementsLen; i++) {
     codeTextElements[i].style.fontFamily = fontFamily;
